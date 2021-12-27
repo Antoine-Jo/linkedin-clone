95ca458bacf2101021f3dca664a5827fd94ec74c
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-27 15:33:05 +0100

    Installation du projet / création du header

---
+++ src/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import logo from './logo-lkdin.svg';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+function Header() {
+    return (
+        <div className='header'>
+           <div className="header__left">
+                <img src={logo} alt=''/>
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+           </div>
+
+           <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title='Accueil' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='Réseau' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Emplois' />
+                <HeaderOption Icon={ChatIcon} title='Messagerie' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+           </div>
+        </div>
+    )
+}
+
+export default Header
