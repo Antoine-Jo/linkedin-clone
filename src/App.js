a47d8233ef55eed157bf9a68406862d5b9f2eba1
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-28 17:00:15 +0100

    Mise en place reducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
+import Login from './Login';
 import SideBar from './SideBar'
 
 function App() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="app">
       <Header />
 
-      <div className='app__body'>
-        <SideBar />
-        <Feed />
-        {/* Widgets */}
-      </div>
+      {!user ? (
+      <Login />
+      ) : (
+        <div className='app__body'>
+          <SideBar />
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
     </div>
   );
 }
