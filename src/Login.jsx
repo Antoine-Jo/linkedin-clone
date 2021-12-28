31590d653b65c52def75107eff1fec5a431c5b69
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-28 17:34:12 +0100

    Création Login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Login.css';
+import linkedin from './linkedin.svg';
 
 function Login() {
     return (
         <div className='login'>
-            
+            <img src={linkedin} alt=''/>
+
+            <form>
+                <input placeholder='Full name (required if registering)' type="text" />
+                
+                <input placeholder='Profile pic URL (optional)' type="text" />
+                
+                <input placeholder='Email' type="email" />
+
+                <input placeholder='Password' type="password" />
+
+                <button>S'identifier</button>
+            </form>
+
+            <p>Pas un membre ?
+                <span> S'inscrire maintenant</span>
+            </p>
         </div>
     )
 }
